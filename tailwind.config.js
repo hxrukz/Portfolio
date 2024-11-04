6cb47fa30294316dc23c2b41216766134663086e
hxrukz <davihsato@gmail.com>
2024-11-04 18:30:22 -0300

    Atualização - Portfolio - 04/11

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,18 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        robotoflex: ["Roboto Flex", "sans-serif"]
+      },
+      colors: {
+        "primary-1": "#2d4865",
+        "primary-2": "#3e5977",
+        "primary-3": "#4f6988",
+        "primary-4": "#607a9a",
+        "primary-5": "#718aab",
+      }
+    },
   },
   plugins: [],
 }
